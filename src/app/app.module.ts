481a3b5a9030e8c5c1d112a04caa06fb4440105d
joeatxantie <joe@xantie.com>
2021-03-17 07:18:54 -0600

    Added dashboard, calendar, filters, navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,12 @@ import { JwtModule } from '@auth0/angular-jwt';
 import { UsersComponent } from './users/users.component';
 import { NavComponent } from './shared/components/nav/nav.component';
 import { NotifierModule } from 'angular-notifier';
-
+import { ChartsModule } from 'ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { DashboardFiltersComponent } from './shared/dashboard-filters/dashboard-filters.component';
+import { CalendarComponent } from './shared/calendar/calendar.component';
+import { DashboardComponent } from './shared/dashboard/dashboard.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +24,11 @@ import { NotifierModule } from 'angular-notifier';
     LoginComponent,
     HomeComponent,
     UsersComponent,
-    NavComponent
+    NavComponent,
+    DashboardFiltersComponent,
+    CalendarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -27,7 +36,9 @@ import { NotifierModule } from 'angular-notifier';
     ReactiveFormsModule,
     HttpClientModule,
     JwtModule,
-    NotifierModule
+    NotifierModule,
+    ChartsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/dashboard/dashboard.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType} from 'chart.js';
+import { Color, Label } from 'ng2-charts';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  lineChartBloodPressureData: ChartDataSets[] = [{ data: [75, 77, 78, 75, 77, 75, 78, 75, 74], label: 'Blood Pressure' },];
+  lineChartHeartRateData: ChartDataSets[] = [{ data: [75, 77, 78, 75, 77, 75, 78, 75, 74], label: 'Heart Rate' },];
+  lineChartTemperatureData: ChartDataSets[] = [{ data: [75, 77, 78, 75, 77, 75, 78, 75, 74], label: 'Temperature' },];
+
+
+  lineChartBloodPressureLabels: Label[] = ['02/01/21', '02/08/21', '02/15/21', '02/22/21', '03/01/21', '02/01/21', '03/08/21', '03/15/21'];
+  lineChartHeartRateLabels: Label[] = ['02/01/21', '02/08/21', '02/15/21', '02/22/21', '03/01/21', '02/01/21', '03/08/21', '03/15/21'];
+  lineChartTemperatureLabels: Label[] = ['02/01/21', '02/08/21', '02/15/21', '02/22/21', '03/01/21', '02/01/21', '03/08/21', '03/15/21'];
+
+  lineChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+  };
+
+  lineChartColors: Color[] = [
+    {
+      borderColor: 'orange',
+      backgroundColor: 'rgba(255,255,255,0.28)',
+    },
+  ];
+
+  lineChartLegend = false;
+  lineChartPlugins = [];
+  lineChartType: ChartType = 'line';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
